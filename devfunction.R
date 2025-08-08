47611e219fb0fd93233efcea5c0b134c833e5da4
agareau <alexandregareau.ag@gmail.com>
2025-08-08 10:30:10 -0400

    commit

--- devfunction.R
+++ devfunction.R
@@ -3,3 +3,10 @@ devtools::document()
 
 # update website
 pkgdown::build_site()
+pkgdown::preview_site()
+
+# Package import and dependencies
+usethis::use_package("tidyverse", type = "depends")
+
+imports <- c("tools", "readxl")
+purrr::walk(imports, usethis::use_package)
